be14843e1050f354241fb4aba2e797bb71b11fab
Callebe <callebex9@gmail.com>
2021-05-17 16:10:54 -0400

    ...

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,1 +1,8 @@
-alert("teste");
+document.querySelector("#inpCPF").addEventListener("keypress", function(evt){
+	console.log(evt.target.value);
+});
+
+
+document.querySelector("#inpCell").addEventListener("keypress", evt => {
+	console.log(evt.target.value);
+});
